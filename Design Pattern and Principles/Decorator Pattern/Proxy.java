357c61033b794eac26608a00b6011d865e12d9b8
PAVITHRA_KARTHI <155638973+PAVITHRA-KALAKARTHI@users.noreply.github.com>
2026-06-23 18:59:47 +0530

    Create Proxy.java

---
+++ Design Pattern and Principles/Decorator Pattern/Proxy.java
@@ -0,0 +1,58 @@
+// Decorator Pattern Example
+public class Proxy {
+    public static void main(String[] args) {
+        // Basic Email Notification
+        Notifier notifier = new EmailNotifier();
+        System.out.println("Email Notification:");
+        notifier.send();
+        // Adding SMS functionality dynamically
+        Notifier smsNotifier = new SMSNotifierDecorator(notifier);
+        System.out.println("\nEmail + SMS Notification:");
+        smsNotifier.send();
+        // Adding Slack functionality dynamically
+        Notifier slackNotifier = new SlackNotifierDecorator(smsNotifier);
+        System.out.println("\nEmail + SMS + Slack Notification:");
+        slackNotifier.send();
+        System.out.println("\nDecorator Pattern Implemented Successfully");
+    }
+}
+// Component Interface
+interface Notifier {
+    void send();
+}
+// Concrete Component
+class EmailNotifier implements Notifier {
+    public void send() {
+        System.out.println("Sending Email Notification");
+    }
+}
+// Abstract Decorator Class
+abstract class NotifierDecorator implements Notifier {
+    protected Notifier notifier;
+    public NotifierDecorator(Notifier notifier) {
+        this.notifier = notifier;
+    }
+    public void send() {
+        notifier.send();
+    }
+}
+// SMS Decorator
+class SMSNotifierDecorator extends NotifierDecorator {
+    public SMSNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+    public void send() {
+        super.send();
+        System.out.println("Sending SMS Notification");
+    }
+}
+// Slack Decorator
+class SlackNotifierDecorator extends NotifierDecorator {
+    public SlackNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+    public void send() {
+        super.send();
+        System.out.println("Sending Slack Notification");
+    }
+}
