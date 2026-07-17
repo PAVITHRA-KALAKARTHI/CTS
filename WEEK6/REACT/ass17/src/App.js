66089de4a1460718bc136f6805a5c98671a3e568
PAVITHRA_KARTHI <155638973+PAVITHRA-KALAKARTHI@users.noreply.github.com>
2026-07-17 11:26:49 +0530

    Add files via upload

---
+++ WEEK6/REACT/ass17/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Getuser from "./Getuser";
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <Getuser />
+
+    </div>
+
+  );
+
+}
+
+export default App;
