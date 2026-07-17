c58da18a4ab1fd1761ce487057f470f3f0098c13
PAVITHRA_KARTHI <155638973+PAVITHRA-KALAKARTHI@users.noreply.github.com>
2026-07-17 14:38:03 +0530

    Add files via upload

---
+++ WEEK6/REACT/ass19/src/GitClient.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+class GitClient {
+
+    getRepositories(username) {
+        return axios.get(`https://api.github.com/users/${username}/repos`);
+    }
+
+}
+
+export default new GitClient();
