a03e4e3b1dd7084bcbba677a2eecda6e4c5f1843
PAVITHRA_KARTHI <155638973+PAVITHRA-KALAKARTHI@users.noreply.github.com>
2026-07-28 14:01:57 +0530

    Add files via upload

---
+++ WEEK7/Angular/Handson6/Task2/src/app/components/course-card/course-card.ts
@@ -0,0 +1,96 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+
+import { HighlightDirective } from '../../directives/highlight.directive';
+import { CreditLabelPipe } from '../../pipes/credit-label.pipe';
+
+import { EnrollmentService } from '../../services/enrollment';
+
+import { Course } from '../../models/course.model';
+
+@Component({
+  selector: 'app-course-card',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HighlightDirective,
+    CreditLabelPipe
+  ],
+  templateUrl: './course-card.html',
+  styleUrl: './course-card.css'
+})
+export class CourseCard implements OnChanges {
+
+  @Input()
+  course!: Course;
+
+  isExpanded = false;
+
+  constructor(
+    public enrollmentService: EnrollmentService
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    console.log(changes);
+
+  }
+
+  toggleDetails(): void {
+
+    this.isExpanded = !this.isExpanded;
+
+  }
+
+  toggleEnrollment(): void {
+
+    if (this.enrollmentService.isEnrolled(this.course.id)) {
+
+      this.enrollmentService.unenroll(this.course.id);
+
+    } else {
+
+      this.enrollmentService.enroll(this.course.id);
+
+    }
+
+  }
+
+  get cardClasses() {
+
+    return {
+
+      'card-enrolled': this.course.enrolled,
+
+      'card-full': this.course.credits >= 4,
+
+      'expanded': this.isExpanded
+
+    };
+
+  }
+
+  get borderColor() {
+
+    switch (this.course.gradeStatus) {
+
+      case 'passed':
+        return 'green';
+
+      case 'failed':
+        return 'red';
+
+      default:
+        return 'gray';
+
+    }
+
+  }
+
+}
---
+++ WEEK7/Angular/Handson6/Task2/src/app/pages/home/home.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CourseService } from '../../services/course.service';
+import { CourseSummary } from '../../components/course-summary/course-summary';
+import { Notification } from '../../components/notification/notification';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    CourseSummary,
+    Notification
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home implements OnInit, OnDestroy {
+
+  portalName = 'Student Course Portal';
+
+  isPortalActive = true;
+
+  message = '';
+
+  searchTerm = '';
+
+  coursesAvailable = 0;
+
+  enrolledCourses = 3;
+
+  gpa = 3.8;
+
+  constructor(
+    private courseService: CourseService
+  ) {}
+
+  ngOnInit(): void {
+
+    this.coursesAvailable = this.courseService.getCourses().length;
+
+    console.log('HomeComponent initialised - courses loaded');
+
+  }
+
+  ngOnDestroy(): void {
+
+    console.log('HomeComponent destroyed');
+
+  }
+
+  onEnrollClick(): void {
+
+    this.message = 'Enrollment opened!';
+
+  }
+
+}
