9cac5987401a7ba121af295a0224435a922bbb44
PAVITHRA_KARTHI <155638973+PAVITHRA-KALAKARTHI@users.noreply.github.com>
2026-07-28 20:41:40 +0530

    Add files via upload

---
+++ WEEK7/Angular/Handson9/Task1/src/app/app.config.ts
@@ -0,0 +1,56 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+
+import {
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
+import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import { routes } from './app.routes';
+
+import { authInterceptor } from './interceptors/auth.interceptor';
+import { errorHandlerInterceptor } from './interceptors/error-handler-interceptor';
+import { loadingInterceptor } from './interceptors/loading-interceptor';
+
+import { courseReducer } from './store/course.reducer';
+
+export const appConfig: ApplicationConfig = {
+
+  providers: [
+
+    provideBrowserGlobalErrorListeners(),
+
+    provideRouter(routes),
+
+    provideHttpClient(
+
+      withInterceptors([
+        authInterceptor,
+        errorHandlerInterceptor,
+        loadingInterceptor
+      ])
+
+    ),
+
+    provideStore({
+
+      courses: courseReducer
+
+    }),
+
+    provideStoreDevtools({
+
+      maxAge: 25
+
+    }),
+
+    provideClientHydration()
+
+  ]
+
+};
---
+++ WEEK7/Angular/Handson9/Task1/src/app/store/course.selectors.ts
@@ -0,0 +1,30 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+import { CourseState } from './course.reducer';
+
+export const selectCourseState =
+  createFeatureSelector<CourseState>('courses');
+
+export const selectAllCourses = createSelector(
+
+  selectCourseState,
+
+  (state: CourseState) => state.courses
+
+);
+
+export const selectLoading = createSelector(
+
+  selectCourseState,
+
+  (state: CourseState) => state.loading
+
+);
+
+export const selectError = createSelector(
+
+  selectCourseState,
+
+  (state: CourseState) => state.error
+
+);
